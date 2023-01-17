f1818864e28916c0b62cc059a44c3625689b96b2
maryyhia <115082902+maryyhia@users.noreply.github.com>
2023-01-17 14:18:02 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+import plotly.express as px 
+
+
+# In[3]:
+
+
+df = pd.read_csv('tweets.csv')
+
+
+# In[5]:
+
+
+st.set_page_config(
+    page_title = 'Real-Time Twitter Sentiment',
+    page_icon = ''
+)
+   
+
+
+# In[6]:
+
+
+# dashboard title
+st.title("Real-Time/Live Twitter Sentiment")
+
+
+# In[ ]:
+
+
+# # Filter section
+# job_filter = st.selectbox('Select Game', pd.unique(df['job'])) #st.sidebar.selectbox (for sidebar)
+
+
+# #twitter: 'topic'
+
+
+# In[ ]:
+
+
+# Create single element container
+placeholder = st.empty()
+
+# Dataframe filter
+# df = df[df['job']==job_filter]
+
+#near real-time simulation
+
+for seconds in range(200): #replace with twitter api live df
+#while True: (infinite loop)
+    
+#     df['age_new'] = df['age'] * np.random.choice(range(1,5))
+#     df['balance_new'] = df['balance'] * np.random.choice(range(1,5))
+
+#     # creating KPIs 
+#     avg_age = np.mean(df['age_new']) 
+
+#     count_married = int(df[(df["marital"]=='married')]['marital'].count() + np.random.choice(range(1,30)))
+    
+#     balance = np.mean(df['balance_new'])
+
+#     with placeholder.container():
+#         # create three columns
+#         kpi1, kpi2, kpi3 = st.columns(3)
+
+#         # fill in those three columns with respective metrics or KPIs 
+#         kpi1.metric(label="Age ⏳", value=round(avg_age), delta= round(avg_age) - 10)
+#         kpi2.metric(label="Married Count 💍", value= int(count_married), delta= - 10 + count_married)
+#         kpi3.metric(label="A/C Balance ＄", value= f"$ {round(balance,2)} ", delta= - round(balance/count_married) * 100)
+
+        # create two columns for charts 
+
+        fig_col1, fig_col2 = st.columns(2)
+#         with fig_col1:
+#             st.markdown("### First Chart")
+#             fig = px.density_heatmap(data_frame=df, y = 'age_new', x = 'marital')
+#             st.write(fig)
+        with fig_col2:
+            st.markdown("### Second Chart")
+            fig2 = px.histogram(data_frame = df, x = 'Sentiment')
+            st.write(fig2)
+        
+        st.markdown("### Detailed Data View")
+        st.dataframe(df)
+        time.sleep(1)
+    #placeholder.empty()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
